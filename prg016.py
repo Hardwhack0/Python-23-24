d9673a464df516a42c9760f6445642fe691dc81e
Hardwhack0 <163640052+Hardwhack0@users.noreply.github.com>
2024-04-12 10:18:42 +0200

    Delete prg016.py

--- prg016.py
+++
@@ -1,9 +0,0 @@
-def calculate_sum(input_list):
-    total = 0
-    for number in input_list:
-        total += number
-    return total
-
-numbers = [1, 2, 3, 4, 5]
-total_sum = calculate_sum(numbers)
-print(total_sum) 
