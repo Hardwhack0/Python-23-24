52bfeb12b257eb02a0b93c31bb7ac2ffbf84845d
Hardwhack0 <163640052+Hardwhack0@users.noreply.github.com>
2024-04-12 10:24:13 +0200

    Delete prg08.py

--- prg08.py
+++
@@ -1,9 +0,0 @@
-def prevod_na_hodiny(dny, hodiny):
-    celkove_hodiny = dny * 24 + hodiny
-    return celkove_hodiny
-
-dny = int(input("Zadej počet dnů: "))
-hodiny = int(input("Zadej počet hodin: "))
-
-vysledek = prevod_na_hodiny(dny, hodiny)
-print(f"{dny} dní a {hodiny} hodin je celkem {vysledek} hodin.")
