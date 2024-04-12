9f8b6887abf6ecbb31ae37c42b8ea6c6ae62d5db
Hardwhack0 <163640052+Hardwhack0@users.noreply.github.com>
2024-04-12 10:18:21 +0200

    Delete prg013.py

--- prg013.py
+++
@@ -1,8 +0,0 @@
-def remainder_division(dividend, divisor):
-    return dividend - ((dividend // divisor) * divisor)
-
-dividend = 20
-divisor = 3
-
-result = remainder_division(dividend, divisor)
-print("Zbytek po dělení", dividend, "a", divisor, "je:", result)
