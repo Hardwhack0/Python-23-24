999eeb8f6624d11bbb540d46f5a773b81244b1e3
Hardwhack0 <163640052+Hardwhack0@users.noreply.github.com>
2024-04-12 10:24:06 +0200

    Delete prg07.py

--- prg07.py
+++
@@ -1,8 +0,0 @@
-start = int(input("Zadejte základní číslo: "))
-end = int(input("Zadejte koncový číslo: "))
-
-for i in range(start, end + 1):
-    if i != end:
-        print(i, end=", ")
-    else:
-        print(i)
