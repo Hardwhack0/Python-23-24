561834449841ae295464f8488c57f6418cf9502d
Hardwhack0 <163640052+Hardwhack0@users.noreply.github.com>
2024-04-12 10:23:26 +0200

    Delete prg04.py

--- prg04.py
+++
@@ -1,8 +0,0 @@
-for row in range(8):
-    for col in range(8):
-        # Zjištění, zda je součet řádku a sloupce sudý nebo lichý
-        if (row + col) % 2 == 0:
-            print('0', end=' ')
-        else:
-            print('1', end=' ')
-    print()
